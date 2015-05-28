17050f3f6c81a6f9e7546b772f3cb382ca859b6d
zeqinjie <1875193628@qq.com>
2015-05-28 20:56:15 +0800

    AddressChoicePickView
    地区选择器

--- AddressChoicePickViewDemo/AppDelegate.m
+++ AddressChoicePickViewDemo/AppDelegate.m
@@ -4,7 +4,7 @@
 //
 //  Created by zhengzeqin on 15/5/28.
 //  Copyright (c) 2015年 zhengzeqin. All rights reserved.
-//
+//  make by 郑泽钦 分享
 
 #import "AppDelegate.h"
 
--- AddressChoicePickViewDemo/ViewController.m
+++ AddressChoicePickViewDemo/ViewController.m
@@ -4,11 +4,12 @@
 //
 //  Created by zhengzeqin on 15/5/28.
 //  Copyright (c) 2015年 zhengzeqin. All rights reserved.
-//
+//  make by 郑泽钦 分享
 
 #import "ViewController.h"
-
+#import "AddressChoicePickerView.h"
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *addressLabel;
 
 @end
 
@@ -17,11 +18,17 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+}
+- (IBAction)tapPress:(UITapGestureRecognizer *)sender {
+    AddressChoicePickerView *addressPickerView = [[AddressChoicePickerView alloc]init];
+    addressPickerView.block = ^(AddressChoicePickerView *view,UIButton *btn,AreaObject *locate){
+        self.addressLabel.text = [NSString stringWithFormat:@"%@",locate];
+        self.addressLabel.textColor = [UIColor blackColor];
+    };
+    [addressPickerView show];
+
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 @end
